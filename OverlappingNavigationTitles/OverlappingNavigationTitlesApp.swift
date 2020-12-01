a7da419fbe3d574ea6fccb23025227a0f8216547
Jonathan <jonathancbadger@gmail.com>
2020-12-01 09:14:21 -0600

    Initial Commit

---
+++ OverlappingNavigationTitles/OverlappingNavigationTitlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  OverlappingNavigationTitlesApp.swift
+//  OverlappingNavigationTitles
+//
+//  Created by Jonathan Badger on 12/1/20.
+//
+
+import SwiftUI
+
+@main
+struct OverlappingNavigationTitlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
